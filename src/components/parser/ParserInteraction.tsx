b2279ded57d526330a49403dcc6d6bdf56121db3
ryanalvita <ryanalvita@yahoo.com>
2026-03-18 22:22:34 +0100

    move from /banks to /images

--- src/components/parser/ParserInteraction.tsx
+++ src/components/parser/ParserInteraction.tsx
@@ -13,36 +13,36 @@ type BankOption = {
 };
 
 const BANK_OPTIONS: BankOption[] = [
-		{
-			id: "abn-amro",
-			name: "ABN AMRO",
-			logo: "/bank-statement-parser/banks/abn_amro_logo.svg",
-			enabled: true,
-		},
-		{
-			id: "ing",
-			name: "ING",
-			logo: "/bank-statement-parser/banks/ing_logo.svg",
-			enabled: false,
-		},
-		{
-			id: "rabobank",
-			name: "Rabobank",
-			logo: "/bank-statement-parser/banks/rabobank_logo.svg",
-			enabled: false,
-		},
-		{
-			id: "asn-bank",
-			name: "ASN Bank",
-			logo: "/bank-statement-parser/banks/asn_logo.svg",
-			enabled: false,
-		},
-		{
-			id: "bunq",
-			name: "bunq",
-			logo: "/bank-statement-parser/banks/bunq_logo.svg",
-			enabled: false,
-		},
+	{
+		id: "abn-amro",
+		name: "ABN AMRO",
+		logo: "/bank-statement-parser/images/abn_amro_logo.svg",
+		enabled: true,
+	},
+	{
+		id: "ing",
+		name: "ING",
+		logo: "/bank-statement-parser/images/ing_logo.svg",
+		enabled: false,
+	},
+	{
+		id: "rabobank",
+		name: "Rabobank",
+		logo: "/bank-statement-parser/images/rabobank_logo.svg",
+		enabled: false,
+	},
+	{
+		id: "asn-bank",
+		name: "ASN Bank",
+		logo: "/bank-statement-parser/images/asn_logo.svg",
+		enabled: false,
+	},
+	{
+		id: "bunq",
+		name: "bunq",
+		logo: "/bank-statement-parser/images/bunq_logo.svg",
+		enabled: false,
+	},
 ];
 
 export default function ParserInteraction() {
